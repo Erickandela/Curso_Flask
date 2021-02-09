73c96bffed455a28852d56510ab7d4903c32e243
Erick Candela <erickandela_rocke@hotmail.com>
2021-02-09 16:55:52 -0600

    Implementación con Firestore

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import request, make_response, redirect, render_template, session, ur
 import unittest
 from app import create_app
 from app.forms import LoginForm
+from app.firestore_service import get_users, get_todos
 
 
 app = create_app()
@@ -40,8 +41,15 @@ def hello():
 
     context = {
          'user_ip': user_ip,
-         'todos' : todos,
+         'todos' : get_todos(user_id=username),
          'username': username
     }
+
+    users = get_users()
+    
+    for user in users:
+        print(user.id)
+        print(user.to_dict()['password'])
+
 
     return render_template('hello.html', **context )
